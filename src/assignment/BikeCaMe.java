9d37e2ae0a52310f4dd0f37b095e4960d5b5e51d
Sabanekishor <146730882+Sabanekishor@users.noreply.github.com>
2023-12-17 17:56:54 +0530

    Update BikeCaMe.java

--- src/assignment/BikeCaMe.java
+++ src/assignment/BikeCaMe.java
@@ -18,3 +18,17 @@ public class BikeCaMe {
 	}
 
 }
+package assignment;
+
+public class Bike {
+	String Brand;
+	String color;
+	int Speed;
+	int Average;
+	
+	void Riding(String content) {
+		System.out.println(content);
+		
+	}
+
+}
