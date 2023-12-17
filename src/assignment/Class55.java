6c1cd7bf8a5a57e41cd0f62180e1bdea2be28f07
ADMIN <ADMIN@DESKTOP-US0QBG5>
2023-12-17 14:31:17 +0530

    two method created

---
+++ src/assignment/Class55.java
@@ -0,0 +1,15 @@
+package assignment;
+
+
+public class Class55 {
+	int i = 20;
+	static int j = 10;
+	
+	void test1() {
+		int i = 20;
+		System.out.println("this is non staic methood");
+	}
+static void test2() {
+	System.out.println("this is static method");
+}
+}
---
+++ src/assignment/Class55R.java
@@ -0,0 +1,13 @@
+package assignment;
+
+public class Class55R {
+	public static void main(String[] args) {
+		
+		Class55.test2();
+		
+		Class55 cla = new Class55();
+		cla.test1();
+		
+	}
+
+}
