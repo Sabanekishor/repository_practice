b3f78dad6dcef4c6507fa74a5d0cb32724596df0
ADMIN <ADMIN@DESKTOP-US0QBG5>
2023-12-17 17:51:52 +0530

    call all variables in other classes after creating objects call methods too and interact with them

---
+++ src/assignment/Bike.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class Bike {
+	String Brand;
+	String color;
+	int Speed;
+	int Average;
+	
+	void Riding(String content) {
+		System.out.println(content);
+		
+	}
+
+}
---
+++ src/assignment/BikeCaMe.java
@@ -0,0 +1,20 @@
+package assignment;
+
+public class BikeCaMe {
+	public static void main(String[] args) {
+		
+		Bike b1 = new Bike();
+		b1.Brand = "KTM";
+		b1.color = "Yellow";
+		b1.Speed = 110;
+		b1.Average = 90;
+		
+		System.out.println("b1.Speed");
+		{
+			Bike b2 = new Bike();
+			b2.Riding("Riding on high way");
+		}
+		
+	}
+
+}
